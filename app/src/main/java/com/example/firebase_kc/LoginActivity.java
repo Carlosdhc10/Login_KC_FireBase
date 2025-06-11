bfe017822ba1adc34ca5759eb98367793084d36c
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-06-11 14:51:07 -0500

    first commit

---
+++ app/src/main/java/com/example/firebase_kc/LoginActivity.java
@@ -0,0 +1,86 @@
+package com.example.firebase_kc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private TextView signupRedirectText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance(); // Inicialización de FirebaseAuth
+
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signUpRedirectText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString().trim();
+                String pass = loginPassword.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    loginEmail.setError("Email incorrecto, intenta otra vez");
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    loginEmail.setError("Coloca un correo válido");
+                    return;
+                }
+
+                if (pass.isEmpty()) {
+                    loginPassword.setError("Intenta otra vez la contraseña");
+                    return;
+                }
+
+                // Autenticación con Firebase
+                auth.signInWithEmailAndPassword(email, pass)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Fallo al iniciar sesión: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SingUpActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/firebase_kc/SingUpActivity.java
@@ -0,0 +1,74 @@
+package com.example.firebase_kc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword;
+    private Button signupButton;
+    private TextView loginRedirectText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+
+                if (user.isEmpty()){
+                    signupEmail.setError("Email invalido");
+                }
+                if (pass.isEmpty()){
+                    signupPassword.setError("Contraseña incorrecta");
+                }else{
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if ( task.isSuccessful()){
+                                Toast.makeText(SingUpActivity.this, "Increso correcto", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SingUpActivity.this, LoginActivity.class));
+                            }else {
+                                Toast.makeText(SingUpActivity.this, "Inicio Fallado" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SingUpActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
